3523718d0bffb5186846d463bb6ee93a9c70fc3d
Tolya Fam <famovkin@yandex.ru>
2021-09-12 17:05:28 +0300

    feat: убрать возможность скролла при попапчике

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,8 @@
 let popup = document.querySelector('.popup');
 let popupCloseBtn = popup.querySelector('.popup__close');
 let popupOpenBtn = document.querySelector('.edit-button');
-
-function popupToggle() {
-  popup.classList.toggle('popup_opened');
-}
-function clickOverlay(event) {
-  if (event.target === event.currentTarget) {
-    popupToggle();
-  }
-}
-
-popup.addEventListener('click', clickOverlay);
-popupOpenBtn.addEventListener('click', popupToggle);
-popupCloseBtn.addEventListener('click', popupToggle);
-
 let likeButton = document.querySelectorAll('.like-button');
-
-for (let i = 0; i < likeButton.length; i++) {
-  function pressLike() {
-    likeButton[i].classList.toggle('like-button_liked');
-  }
-  likeButton[i].addEventListener('click', pressLike);
-}
-
+let page = document.querySelector('.page')
 let formElement = document.querySelector('.popup__form');
 let profileName = document.querySelector('.profile__name');
 let job = document.querySelector('.profile__job');
@@ -33,11 +12,35 @@ let jobInput = formElement.querySelector('.popup__input_type_job');
 nameInput.value = profileName.textContent;
 jobInput.value = job.textContent;
 
-  function formSubmitHandler(evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    job.textContent = jobInput.value;
+for (let i = 0; i < likeButton.length; i++) {
+  function pressLike() {
+    likeButton[i].classList.toggle('like-button_liked');
+  }
+  likeButton[i].addEventListener('click', pressLike);
+}
+
+function popupToggle() {
+  popup.classList.toggle('popup_opened');
+  page.classList.toggle('page_no-scroll');
+}
+
+function clickOverlay(event) {
+  if (event.target === event.currentTarget) {
     popupToggle();
   }
+}
 
-formElement.addEventListener ('submit', formSubmitHandler);
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  job.textContent = jobInput.value;
+  popupToggle();
+}
+
+popup.addEventListener('click', clickOverlay);
+popupOpenBtn.addEventListener('click', popupToggle);
+popupCloseBtn.addEventListener('click', popupToggle);
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
