322052b2933262f5daf1701fbe77990bf05b505b
Tolya Fam <famovkin@yandex.ru>
2021-09-12 03:59:20 +0300

    feat: добавить toggle для кнопки like

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,31 @@
 let popup = document.querySelector('.popup');
 let popupCloseBtn = popup.querySelector('.popup__close');
 let popupOpenBtn = document.querySelector('.edit-button');
+let likeButton = document.querySelectorAll('.like-button');
 
 function popupToggle() {
   popup.classList.toggle('popup_opened');
 };
 
+function pressLike() {
+  likeButton[i].classList.toggle('like-button_liked')
+}
+
+for(let i = 0; i < likeButton.length; i++) {
+  function pressLike() {
+    likeButton[i].classList.toggle('like-button_liked');
+  }
+  likeButton[i].addEventListener('click', pressLike);
+}
+
 function clickOverlay(event) {
-  debugger
   if (event.target === event.currentTarget) {
     popupToggle();
   };
 };
 
-popup.addEventListener('click', clickOverlay)
+popup.addEventListener('click', clickOverlay);
 popupOpenBtn.addEventListener('click', popupToggle);
 popupCloseBtn.addEventListener('click', popupToggle);
+
+
