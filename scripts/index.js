96d7aeb800f042ef5628fa338ebadb0eaa1e7f0a
Tolya Fam <famovkin@yandex.ru>
2021-09-12 05:44:04 +0300

    feat: добавить сохранение и отображение текущих полей в форме

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,44 @@
 let popup = document.querySelector('.popup');
 let popupCloseBtn = popup.querySelector('.popup__close');
 let popupOpenBtn = document.querySelector('.edit-button');
-let likeButton = document.querySelectorAll('.like-button');
 
 function popupToggle() {
   popup.classList.toggle('popup_opened');
-};
-
-function pressLike() {
-  likeButton[i].classList.toggle('like-button_liked')
+}
+function clickOverlay(event) {
+  if (event.target === event.currentTarget) {
+    popupToggle();
+  }
 }
 
-for(let i = 0; i < likeButton.length; i++) {
+popup.addEventListener('click', clickOverlay);
+popupOpenBtn.addEventListener('click', popupToggle);
+popupCloseBtn.addEventListener('click', popupToggle);
+
+let likeButton = document.querySelectorAll('.like-button');
+
+for (let i = 0; i < likeButton.length; i++) {
   function pressLike() {
     likeButton[i].classList.toggle('like-button_liked');
   }
   likeButton[i].addEventListener('click', pressLike);
 }
 
-function clickOverlay(event) {
-  if (event.target === event.currentTarget) {
+let formElement = document.querySelector('.popup__form');
+let submitButton = formElement.querySelector('.popup__button');
+let profileName = document.querySelector('.profile__name');
+let job = document.querySelector('.profile__job');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_job');
+
+nameInput.value = profileName.textContent;
+jobInput.value = job.textContent;
+
+  function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    job.textContent = jobInput.value;
     popupToggle();
-  };
-};
+  }
 
-popup.addEventListener('click', clickOverlay);
-popupOpenBtn.addEventListener('click', popupToggle);
-popupCloseBtn.addEventListener('click', popupToggle);
-
-
+submitButton.addEventListener ('click', formSubmitHandler);
