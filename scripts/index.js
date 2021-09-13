f70da679df8dbd0a94f7f10d811310165393a0d7
Tolya Fam <famovkin@yandex.ru>
2021-09-13 16:12:45 +0300

    refactor: удалить строки не по заданию из index.js, подкорректировать readme, убрать лишние подключения в index.css

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,6 @@
 let popup = document.querySelector('.popup');
 let popupCloseBtn = popup.querySelector('.popup__close');
 let popupOpenBtn = document.querySelector('.edit-button');
-let likeButton = document.querySelectorAll('.like-button');
-let page = document.querySelector('.page')
 let formElement = document.querySelector('.popup__form');
 let profileName = document.querySelector('.profile__name');
 let job = document.querySelector('.profile__job');
@@ -12,22 +10,8 @@ let jobInput = formElement.querySelector('.popup__input_type_job');
 nameInput.value = profileName.textContent;
 jobInput.value = job.textContent;
 
-for (let i = 0; i < likeButton.length; i++) {
-  function pressLike() {
-    likeButton[i].classList.toggle('like-button_liked');
-  }
-  likeButton[i].addEventListener('click', pressLike);
-}
-
 function popupToggle() {
   popup.classList.toggle('popup_opened');
-  page.classList.toggle('page_no-scroll');
-}
-
-function clickOverlay(event) {
-  if (event.target === event.currentTarget) {
-    popupToggle();
-  }
 }
 
 function formSubmitHandler(evt) {
@@ -37,7 +21,6 @@ function formSubmitHandler(evt) {
   popupToggle();
 }
 
-popup.addEventListener('click', clickOverlay);
 popupOpenBtn.addEventListener('click', popupToggle);
 popupCloseBtn.addEventListener('click', popupToggle);
 formElement.addEventListener('submit', formSubmitHandler);
