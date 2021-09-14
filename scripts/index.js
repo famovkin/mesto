fd4cba01d7c040a783737344746eb3042cc166eb
Tolya Fam <famovkin@yandex.ru>
2021-09-14 21:06:16 +0300

    fix: убрать функцию с toggle, разбить ее на отдельные функции с открытием/закрытием попапа, добавить комментарии в код

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,32 @@
-let popup = document.querySelector('.popup');
-let popupCloseBtn = popup.querySelector('.popup__close');
-let popupOpenBtn = document.querySelector('.edit-button');
-let formElement = document.querySelector('.popup__form');
-let profileName = document.querySelector('.profile__name');
-let job = document.querySelector('.profile__job');
-let nameInput = formElement.querySelector('.popup__input_type_name');
-let jobInput = formElement.querySelector('.popup__input_type_job');
+let popup = document.querySelector('.popup'); // находим div с классом popup
+let popupCloseBtn = popup.querySelector('.popup__close-button'); // находим кнопку закрытия
+let popupOpenBtn = document.querySelector('.profile__edit-button'); // находим кнопку редактирования
+let formElement = document.querySelector('.popup__form'); // находим форму
+let profileName = document.querySelector('.profile__name'); // находим заголовок с именем
+let job = document.querySelector('.profile__job'); // находим параграф с работой
+let nameInput = formElement.querySelector('.popup__input_type_name'); // находим инпут с именем
+let jobInput = formElement.querySelector('.popup__input_type_job'); // находим инпут с работой
 
-nameInput.value = profileName.textContent;
-jobInput.value = job.textContent;
-
-function popupToggle() {
-  popup.classList.toggle('popup_opened');
+function openPopup() { // функция открытия попапчика
+  popup.classList.add('popup_opened'); // попап становится видимым из-за display: flex
+  nameInput.value = profileName.textContent; // содержимое из имени(h1) и работы(p) присвается соответствующим инпутам
+  jobInput.value = job.textContent;
 }
 
-function formSubmitHandler(evt) {
+function closePopup() { // функция закрытия попапчика
+  popup.classList.remove('popup_opened'); // попап снова невидим из-за display: none
+}
+
+function formSubmitHandler(evt) { // функция сохранения значений из инпутов
   evt.preventDefault();
-  profileName.textContent = nameInput.value;
+  profileName.textContent = nameInput.value; // h1 и p присваиваются соответствующие значения из инпутов
   job.textContent = jobInput.value;
-  popupToggle();
+  closePopup();
 }
 
-popupOpenBtn.addEventListener('click', popupToggle);
-popupCloseBtn.addEventListener('click', popupToggle);
-formElement.addEventListener('submit', formSubmitHandler);
+popupOpenBtn.addEventListener('click', openPopup); // слушатель на кнопке редактировать
+popupCloseBtn.addEventListener('click', closePopup); // слушатель на кнопке закрыть
+formElement.addEventListener('submit', formSubmitHandler); // слушатель на форме
 
 
 
