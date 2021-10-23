fa4114168f6aadc03f476ede20b11c9817445168
Tolya Fam <famovkin@yandex.ru>
2021-10-23 17:28:16 +0300

    refactor: добавить в класс Card методы для слушателей и обработчиков событий

--- scripts/Card.js
+++ scripts/Card.js
@@ -17,11 +17,36 @@ export default class Card {
 
   generateCard() {
     this._element = this._getTemplate();
+    this._setEventListeners();
 
     this._element.querySelector('.card__title').textContent = this._name;
     this._element.querySelector('.card__image').src = this._link;
     this._element.querySelector('.card__image').alt = this._name;
 
     return this._element;
+  }
+
+  _handleDeleteBtnClick() {
+    this._element.remove();
+  }
+
+  _handleImageClick() {
+    document.querySelector('.popup_type_image').classList.add('popup_opened');
+  }
+
+  _handleLikeBtnClick() {
+    this._element.querySelector('.card__like-button').classList.toggle('card__like-button_type_liked');
+  }
+
+  _setEventListeners() {
+    this._element.querySelector('.card__delete-button').addEventListener('click', () => {
+      this._handleDeleteBtnClick();
+    });
+    this._element.querySelector('.card__image').addEventListener('click', () => {
+      this._handleImageClick();
+    });
+    this._element.querySelector('.card__like-button').addEventListener('click', () => {
+      this._handleLikeBtnClick();
+    });
   }
 }
