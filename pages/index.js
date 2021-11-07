6e7c59aa8283190a9bf945f3cf0b02f085b91f63
Tolya Fam <famovkin@yandex.ru>
2021-11-07 13:26:47 +0300

    refactor: удалить код из index.js, связанный с открытием/закрытием попапов

--- pages/index.js
+++ pages/index.js
@@ -25,51 +25,11 @@ import FormValidator from '../scripts/components/FormValidator.js';
 import Section from '../scripts/components/Section.js';
 import PopupWithImage from '../scripts/components/PopupWithImage.js';
 
-function closeByEsc(evt) {
-  if (evt.key === ESC_CODE) {
-    const openedPopup = document.querySelector('.popup_opened');
-    closePopup(openedPopup);
-  }
-}
-
-function closePopup(modal) { // функция закрытия попапа
-  modal.classList.remove('popup_opened'); // удаление класса, который отображает попап
-  window.removeEventListener('keydown', closeByEsc);
-}
-
-function openPopup(modal) { // функция открытия попапа, в параметр modal передаем тип попапа
-  modal.classList.add('popup_opened'); // попап становится видимым из-за display: flex
-  window.addEventListener('keydown', closeByEsc);
-}
-
-popups.forEach((popup) => {
-  popup.addEventListener('click', (evt) => {
-    if (evt.target.classList.contains('popup_opened')) {
-      closePopup(popup);
-    }
-    if (evt.target.classList.contains('popup__close-image')) {
-      closePopup(popup);
-    }
-  });
-});
-
 const validatorForFormEdit = new FormValidator (config, formEdit);
 validatorForFormEdit.enableValidation();
 
 const validatorForFormAdd =  new FormValidator (config, formAdd);
 validatorForFormAdd.enableValidation();
-
-popupAddOpenBtn.addEventListener('click', () => {
-  validatorForFormAdd.resetValidation();
-  openPopup(popupAdd);
-});
-
-popupEditOpenBtn.addEventListener('click', () => {
-  validatorForFormEdit.resetValidation();
-  openPopup(popupEdit);
-  nameInput.value = profileName.textContent; // содержимое из имени(h1) и работы(p) присвоится соответствующим инпутам
-  jobInput.value = job.textContent;
-});
 
 function addListenerToSubmitForm(form, submitFunc) { // фукнция добавления слушателя для submit формы
   form.addEventListener('submit', submitFunc); // form - сама форма, submitFunc - функция, которая выполнится
