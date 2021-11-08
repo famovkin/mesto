d5bd1fa1a384ed70f0f15cd4ad5616c19a918de7
Tolya Fam <famovkin@yandex.ru>
2021-11-08 23:11:35 +0300

    refactor: в классе PopupWithForm перенести в конструктор поле _inputList, а _submitListenerBind удалить

--- src/scripts/components/PopupWithForm.js
+++ src/scripts/components/PopupWithForm.js
@@ -4,11 +4,8 @@ export default class PopupWithForm extends Popup {
   constructor({ popupSelector, handleFormSubmit }) {
     super(popupSelector);
     this._form = this._popup.querySelector('.popup__form');
+    this._inputList = this._popup.querySelectorAll('.popup__input');
     this._handleFormSubmit = handleFormSubmit;
-    this._submitListenerBind = (evt) => {
-      evt.preventDefault();
-      this._handleFormSubmit(this._getInputValues());
-    }
   }
 
   close() {
@@ -17,17 +14,18 @@ export default class PopupWithForm extends Popup {
   }
 
   _getInputValues() {
-    this._inputList = this._popup.querySelectorAll('.popup__input');
     this._formValues = {};
     this._inputList.forEach(input => {
       this._formValues[input.name] = input.value;
     });
-
     return this._formValues;
   }
 
   setEventListeners() {
-    this._form.addEventListener('submit', this._submitListenerBind);
+    this._form.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+    });
     super.setEventListeners();
   }
 }
